48da6d28a981a043e5cfa3bd8a7f49d30e421927
Philipp Hoefflin <hoefflin@fuh-e.de>
2014-01-13 18:21:13 +0100

    PostgreSQL - use TO_CHAR instead of CAST

--- app/models/tt_completer.rb
+++ app/models/tt_completer.rb
@@ -10,7 +10,7 @@ class TtCompleter
     issue_list = Array.new
     if term.match(/^\d+$/)
       issue_list << Issue.visible.find_by_id(term.to_i)
-      issue_list += Issue.visible.where(["LOWER(CAST(#{Issue.table_name}.id as CHAR)) LIKE ?", "%#{term.downcase}%"]).all
+      issue_list += Issue.visible.where(["LOWER(to_char(#{Issue.table_name}.id,'999')) LIKE ?", "%#{term.downcase}%"]).all
     end
     unless term.blank?
       issue_list += Issue.visible.where(["LOWER(#{Issue.table_name}.subject) LIKE ?", "%#{term.downcase}%"]).all
